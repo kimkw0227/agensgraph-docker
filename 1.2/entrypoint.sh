e2eb5217865e45393f8a23f7fd87a6940faea9ae
kimkw0227 <kimkw0227@gmail.com>
2017-09-29 21:07:56 -0700

    Update entrypoint.sh

--- 1.2/entrypoint.sh
+++ 1.2/entrypoint.sh
@@ -4,7 +4,7 @@ tag="1.2.0"
 
 rm /tmp/*.pid &> /dev/null
 mode=$1
-usage="\n******************************************************************************\n\nTo run AgensGraph container:  \n\n=> docker run -d -p 5432:5432 -p 8085:8085 kimkw0227/agensgraph agens-graph \n\nTo access the container via bash: \n\n=> docker exec -it [YOUR.CONTAINER.NAME.HERE] /bin/bash \n\nTo access the container via agens shell: \n\n=> docker exec -it [YOUR.CONTAINER.NAME.HERE] agens \n\n******************************************************************************"
+usage="\n******************************************************************************\n\nTo run AgensGraph container:  \n\n=> docker run -d -p 5432:5432 kimkw0227/agensgraph agens-graph \n\nTo access the container via bash: \n\n=> docker exec -it [YOUR.CONTAINER.NAME.HERE] /bin/bash \n\nTo access the container via agens shell: \n\n=> docker exec -it [YOUR.CONTAINER.NAME.HERE] agens \n\n******************************************************************************"
 
 function main(){
     case $mode in
